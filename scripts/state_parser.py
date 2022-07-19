1fac546ea2e4d6cc61fa8befe2af1d5316ea8afa
ElDiabloRojo <holdens.uk@gmail.com>
2022-07-19 17:30:40 +0100

    init func & multiprocessing

--- scripts/state_parser.py
+++ scripts/state_parser.py
@@ -4,6 +4,8 @@ import logging
 import re
 import os
 from jinja2 import Environment, FileSystemLoader
+from subprocess import Popen, PIPE, STDOUT
+from multiprocessing import Pool
 
 def configure_logging(logging_level):
   logging.basicConfig(
@@ -40,6 +42,7 @@ def parse_bucket_objects(bucket_name, search_string):
   
   return matching_objects
 
+
 def parse_res_classes(state_file):
   """
   Returns a list of res_classes, by taking the state file name and stripping
@@ -57,7 +60,7 @@ def parse_res_classes(state_file):
   return res_class_name
 
 
-def generate_backend_file(bucket_name, state_file_list, aws_region, project_environment):
+def generate_backend_file(bucket_name, state_file_list, aws_region, output_path):
   file_loader = FileSystemLoader('templates')
   env = Environment(loader=file_loader)
 
@@ -65,7 +68,7 @@ def generate_backend_file(bucket_name, state_file_list, aws_region, project_envi
 
   for state_file in state_file_list:
     res_class = parse_res_classes(state_file)
-    output_dir = "./generated/" + project_environment + "/" + res_class + "/"
+    output_dir = output_path + res_class + "/"
     output_file_name = "backend.tf"
     output_uri = output_dir + output_file_name
 
@@ -79,6 +82,54 @@ def generate_backend_file(bucket_name, state_file_list, aws_region, project_envi
     
     logging.info("Generated file: '{}'".format(output_uri))
 
+
+def init_res_classes(output_path):
+  res_class_dirs = list()
+  
+  for root, dirs, files in os.walk(output_path, topdown=False):
+    for name in dirs:
+      res_class_dirs.append(os.path.join(root, name))
+
+  multiprocessing_processes = len(res_class_dirs)
+
+  with Pool(multiprocessing_processes) as pool:
+    processed = pool.map(init_subprocess, res_class_dirs)
+
+
+def init_subprocess(value):
+  process = ['/usr/local/bin/terraform', 'init']
+  working_directory = value
+
+  logging.info("Initialising: {}".format(working_directory))
+  proc = Popen(
+    process, 
+    bufsize=8192, 
+    stdin=None, 
+    stdout=PIPE, 
+    stderr=STDOUT, 
+    cwd=working_directory)
+  
+  output = []
+  proc.wait()
+
+  while True:
+
+    proc.poll()
+
+    stdout, stderr = proc.communicate()
+    if stdout:
+      output.append(stdout.decode('utf-8'))
+    if stderr:
+      output.append(stderr.decode('utf-8'))
+
+    if proc.returncode is not None:
+        break
+    time.sleep(0.01)
+
+    if proc.returncode != 0:
+        raise Exception("Error initializing terraform")
+
+    logging.info(output)
 
 def main():
   parser = configparser.ConfigParser()
@@ -94,15 +145,21 @@ def main():
   s3 = boto3.resource('s3')
   state_bucket = s3.Bucket(bucket_name)
 
-  for project in project_environments.split(','):
-    logging.info("Processing project: {}".format(project))
-    state_file_list = list(parse_bucket_objects(state_bucket, project))
+  for environment in project_environments.split(','):
+    output_path = "./generated/" + environment + "/"
 
+    logging.info("Processing project: {}".format(environment))
+    state_file_list = list(parse_bucket_objects(state_bucket, environment))
+
+    logging.info("Beginning generate backend files.")
     generate_backend_file(
       bucket_name, 
       state_file_list, 
       aws_region, 
-      project)
+      output_path)
+    
+    logging.info("Beginning initialise res_classes.")
+    init_res_classes(output_path)
 
 
 if __name__ == "__main__":
